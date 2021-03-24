9c24f4c13ded6e1cbdbc815a3b3bce323307a88c
majunchen <majunchen@bytedance.com>
2021-03-25 00:55:55 +0800

    init hook lesson

--- apps/toutiao-lite-web/pages/_app.tsx
+++ apps/toutiao-lite-web/pages/_app.tsx
@@ -20,6 +20,7 @@ export default function MyApp({ Component, pageProps }) {
 
 MyApp.getInitialProps = async (appContext) => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  
   const appProps = await App.getInitialProps(appContext);
 
   return { ...appProps };
