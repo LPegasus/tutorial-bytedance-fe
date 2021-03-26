df1dd1b3f6d7d3c73111ed0f19b7ca4221aa99e2
majunchen <majunchen@bytedance.com>
2021-03-27 01:16:00 +0800

    add spring gesture demo

--- apps/lesson-4-cra/src/App.tsx
+++ apps/lesson-4-cra/src/App.tsx
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
+import { animated, useSpring } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
 
 function App() {
+  // @ts-ignore
+  const [{ n }, springRef] = useSpring(() => ({ n: 0 }));
+  const bind = useDrag(
+    (state) => {
+      const { movement } = state;
+      springRef.set({ n: movement[0] });
+    },
+    {
+      axis: 'x',
+    }
+  );
+
   return (
-    <div className="App">
+    <div className="App" {...bind()}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <animated.img
+          onMouseDown={(e) => e.preventDefault()}
+          style={{
+            cursor: 'pointer',
+            userSelect: 'none',
+            touchAction: 'none',
+            transform: n.to((x) => `rotateZ(${x}deg)`),
+          }}
+          width="50%"
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- libraries/eslint-config/index.js
+++ libraries/eslint-config/index.js
@@ -2,4 +2,8 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   extends: ['react-app'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+  },
 };
